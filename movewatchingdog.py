7c3aac400dd2d19cde32b5583bd7d3fb575e7be0
Yiming Wang <me@prunusmume.io>
2024-10-30 16:50:03 +0800

    Update movewatchingdog.py

--- movewatchingdog.py
+++ movewatchingdog.py
@@ -24,11 +24,12 @@ moveGetLen = len(moveJson['query']['logevents'])
 moveLen = list(range(moveGetLen))
 originalPages = []
 targetPages = []
+
 for x in moveLen:
     originalPages.append(str(moveJson['query']['logevents'][x]['title']))
     targetPages.append(str(moveJson['query']['logevents'][x]['params']['target_title']))
 
-async def main(originalPage, redirectPages, targetPage):
+async def main():
     bot = Bot(
         sitename = "zh.wikipedia",
         api = "https://zh.wikipedia.org/w/api.php",
@@ -37,20 +38,23 @@ async def main(originalPage, redirectPages, targetPage):
         password = ""
     )
     await bot.login()
-    # Get some informations so that we can avoid edit repeatly.
-    
-    if redirectPages != False:
-        for page in redirectPages:
-            for originalPage in originalPages:
-                for targetPage in targetPages:
-                    text = await bot.get_page_text(title=page)
-                    if str(targetPage) in text:
-                        return
-                    else:
-                        pagetext = await bot.get_page_text(title = page)
-                        pagetext = pagetext.replace(originalPage, targetPage)
-    else:
-        pass
+    for originalPage in originalPages:
+        redirectPages = getRedirect(originalPage)
+        if redirectPages != False:
+            for page in redirectPages:
+                if redirectPages != False:
+                    for targetPage in targetPages:
+                        text = await bot.get_page_text(title=page)
+                        if str(targetPage) in text:
+                            return
+                        else:
+                            pagetext = await bot.get_page_text(title = page)
+                            editext = "#重定向[[{target}]]".format(target = targetPage)
+                            await bot.edit_page(title=page,text=editext,summary="Bot: 修复重定向-自{from1}至{to}-任务码01".format(from1=originalPage, to=targetPage))
+            else:
+                pass
+        else:
+            pass
 
 while True:
     asyncio.run(main())
